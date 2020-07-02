1967cf55afee666c95e567e8eb5cbd138db83f9c
Daniel Kim <berzzubunny@gmail.com>
2020-07-03 00:26:48 +0900

    feat: modify model at games app

--- gamesapi/games/models.py
+++ gamesapi/games/models.py
@@ -1,12 +1,68 @@
 from django.db import models
 
 
+class GameCategory(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
 class Game(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=200, blank=True, default='')
     release_date = models.DateTimeField()
-    game_category = models.CharField(max_length=20, blank=True, default='')
+    game_category = models.ForeignKey(
+        GameCategory,
+        on_delete=models.CASCADE,
+        related_name='games'
+    )  # using ForeignKey
     played = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('name',)
+
+    def __str__(self):      # 각 모델 class의 제목을 무엇으로 할지 반환
+        return self.name
+
+
+class Player(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICES = (
+        (MALE, 'MALE'),
+        (FEMALE, 'FEMALE'),
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50, blank=True, default='')
+    gender = models.CharField(
+        max_length=2,
+        choices=GENDER_CHOICES,
+        default=MALE,
+    )
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class PlayerScore(models.Model):
+    player = models.ForeignKey(
+        Game,
+        related_name='scores',
+        on_delete=models.CASCADE
+    )
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE
+    )
+    score = models.IntegerField()
+    score_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ('-score',)
