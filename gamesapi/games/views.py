0cbd1143e00e9233a94d9f88ca438961fd10f690
Daniel Kim <berzzubunny@gmail.com>
2020-07-04 23:57:22 +0900

    Using GenericView

--- gamesapi/games/views.py
+++ gamesapi/games/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
-from games.models import Game
-from games.serializers import GameSerializer
+from games.models import Game, GameCategory
+from games.serializers import GameSerializer, GameCategorySerializer
 
 
 ## using JSONResponse HttpResponse Class
@@ -58,40 +58,67 @@ from games.serializers import GameSerializer
 #         return HttpResponse(status=status.HTTP_204_NO_CONTENT)  # json으로 반환할 필요가 없으므로 HttpResponse로 반환
 
 
-# using API_VIEW
-@api_view(['GET', 'POST'])
-def game_list(request):
-    if request.method == 'GET':
-        games = Game.objects.all()
-        games_serializer = GameSerializer(games, many=True)
-        return Response(games_serializer.data)
+# # using API_VIEW
+# @api_view(['GET', 'POST'])
+# def game_list(request):
+#     if request.method == 'GET':
+#         games = Game.objects.all()
+#         games_serializer = GameSerializer(games, many=True)
+#         return Response(games_serializer.data)
+#
+#     elif request.method == 'POST':
+#         game_serializer = GameSerializer(data=request.data)
+#         if game_serializer.is_valid():
+#             game_serializer.save()
+#             return Response(game_serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def game_detail(request, pk):
+#     try:
+#         games = Game.objects.get(pk=pk)
+#     except Game.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     if request.method == 'GET':
+#         game_serializer = GameSerializer(games)
+#         return Response(game_serializer.data)
+#
+#     elif request.method == 'PUT':
+#         game_serializer = GameSerializer(data=request.data)
+#         if game_serializer.is_valid():
+#             game_serializer.save()
+#             return Response(game_serializer.data)
+#         return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     elif request.method == 'DELETE':
+#         Game.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    elif request.method == 'POST':
-        game_serializer = GameSerializer(data=request.data)
-        if game_serializer.is_valid():
-            game_serializer.save()
-            return Response(game_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Using GenericView
+class GameCategoryList(generics.ListCreateAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-list'
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def game_detail(request, pk):
-    try:
-        games = Game.objects.get(pk=pk)
-    except Game.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class GameCategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GameCategory.objects.all()
+    serializer_class = GameCategorySerializer
+    name = 'gamecategory-datail'
 
-    if request.method == 'GET':
-        game_serializer = GameSerializer(games)
-        return Response(game_serializer.data)
 
-    elif request.method == 'PUT':
-        game_serializer = GameSerializer(data=request.data)
-        if game_serializer.is_valid():
-            game_serializer.save()
-            return Response(game_serializer.data)
-        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class GameList(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer()
+    name = 'game-list'
 
-    elif request.method == 'DELETE':
-        Game.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    name = 'game-detail'
+
+
